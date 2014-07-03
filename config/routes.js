8ada0a954b01705128e3fcc9162d7a25dfe5dea4
Donald Zhan <donald.zhan1984@gmail.com>
2014-07-04 07:55:33 +0800

    turning format and use fake data

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var main = require('../app/controllers/main');
+var main = require('../app/controllers/main'),
+    api = require('../app/controllers/api');
 
 module.exports = function(app,config){
+    app.get('/api/citymap',api.cityMap);
+
     app.get('/',main.index);
 };
