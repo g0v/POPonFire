3f8d5be70d4eebc4809868875a083e5eee9a3c14
Donald Zhan <donald.zhan1984@gmail.com>
2014-07-23 22:51:18 +0800

    fake piece map api

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -5,43 +5,53 @@ exports.onFire = function(req,res,next) {
     // Fake JSON Data
     var firePoints = {};
 
-    firePoints['taipei101'] = {
-        "addr": 'Taipei 101',
-        "area": 100
-    };
+    firePoints = {
+        
+        'taipei101': {
+            "addr": 'Taipei 101',
+            "area": 100
+        },
 
-    firePoints['president'] = {
-        "addr": '中華民國總統府',
-        "area": 10
-    };
+        'president': {
+            "addr": '中華民國總統府',
+            "area": 10
+        },
 
-    firePoints['ntuh'] = {
-        "addr": '臺大醫院',
-        "area": 50
-    };
+        'ntuh': {
+            "addr": '臺大醫院',
+            "area": 50
+        },
 
-    firePoints['somewhere']  = {
-        "addr": "新北市新店區青潭段楣子寮小段0095-0002地號",
-        "area": 719.22
+        'somewhere': {
+            "addr": "新北市新店區青潭段楣子寮小段0095-0002地號",
+            "area": 719.22
+        }
     }; 
 
     res.send(firePoints);
 };
 
 exports.toSearch = function(req, res, next) {
+    var city = req.query.city;
+    var devWay = req.query.devWay;
+    var unit = req.query.unit;
 
     // TODO : need a search engine for catch data
 
     // Fake search result JSON data
     var result = {
-        
+
         "item00": {
             "addr": '臺大醫院',
             "area": 50
         },
-         "item01": {
+        "item01": {
             "addr": "新北市新店區青潭段楣子寮小段0095-0002地號",
             "area": 719.22
+        },
+        "item02": {
+            "addr": "新北市三峽區隆恩段0292-0000地號",
+            "area": 473.38
         }
     };
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,27 @@
+
+var search = function(city, devWay, unit){              
+
+    // initial the query condition
+    var condition = {
+        "city": city,
+        "devWay": devWay,
+        "unit": unit
+    };
+
+    // send the query to api/search
+    $.getJSON('/api/search', condition, function(searchJSON) {
+        // clear all piece maps
+        $('.piece-column').remove();
+
+        // render the piece map
+        createPieceMap(searchJSON);
+    });
+};
+
 (function(){
 
   // catch the onFire json data from API
   $.getJSON('/api/onfire', function(onFireJSON){
-      console.log("entry get city map json");
       createOnFireMap(onFireJSON);
   });
 
@@ -10,8 +29,15 @@
       createPieceMap(searchJSON);
   });
 
-
   // intial the dropdown items
-  $('.ui.dropdown').dropdown();
+  $('.ui.dropdown').dropdown({
+        onChange: function(){
+            var city = $("input[name=city]").val();
+            var devWay = $("input[name=devWay]").val();
+            var unit = $("input[name=unit]").val();
+
+            search(city, devWay, unit);
+        }
+  });
   
 })();
